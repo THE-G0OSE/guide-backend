986619e0c308c7ffc86a4d424ef34edf488305f0
THE_GOOSE <nimitos54345@gmail.com>
2025-07-25 14:16:25 +0300

    ci/cd test

--- handlers/ping.go
+++ handlers/ping.go
@@ -3,5 +3,5 @@ package handlers
 import "github.com/labstack/echo/v4"
 
 func Ping(c echo.Context) error {
-	return c.JSON(200, echo.Map{"status": "running"})
+	return c.JSON(200, echo.Map{"status": "ok"})
 }
